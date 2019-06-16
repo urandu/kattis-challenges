801abeced0566bb915d135c043432919a92ada07
bildad namawa <bildadnamawa@gmail.com>
2019-06-16 15:19:58 +0300

    oddities problem done

---
+++ oddities/oddities.py
@@ -0,0 +1,8 @@
+test_cases = int(input().strip())
+
+for i in range(test_cases):
+    input1 = int(input().strip())
+    if (input1 % 2) is 0:
+        print("{} is even".format(input1))
+    else:
+        print("{} is odd".format(input1))
