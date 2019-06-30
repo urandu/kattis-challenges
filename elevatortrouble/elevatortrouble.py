f716026eb2f5f15f877d5dbcb87d0f0c3d0a8063
bildad namawa <bildadnamawa@gmail.com>
2019-06-30 17:30:38 +0300

    elavator trouble giving me trouble

--- elevatortrouble/elevatortrouble.py
+++ elevatortrouble/elevatortrouble.py
@@ -1,28 +1,55 @@
-no_of_states = list(range(1, 11))
-goal = 1
+inputs = input().strip().split(" ")
 
-start = 5
 
-steps_right = 1
-steps_left = 1
+no_of_states = int(inputs[0])
+
+# no_of_states = list(range(1, no_of_states+1))
+start = int(inputs[1])
+goal = int(inputs[2])
+steps_right = int(inputs[3])
+steps_left = int(inputs[4])
 es = 0
-print(no_of_states)
-# exit()
+
 steps = 0
 while start != goal and es == 0:
+
     if start < goal:
         distance = goal - start
-        mod_distance = distance % steps_right
-
-        if mod_distance == 0:
-            steps = steps + (distance/steps_right)
-            start = goal
+        if steps_right == 0:
+            es = 1
         else:
-            steps = (distance-mod_distance)/steps_right
-            start = goal - mod_distance
-            if mod_distance == distance:
-                es = 1
-    else:
+            mod_distance = distance % steps_right
 
+            if mod_distance == 0:
+                steps = steps + (distance/steps_right)
+                start = goal
+            elif mod_distance == distance:
+                if (start + steps_right) > no_of_states:
+                    if start <= steps_left:
+                        es = 1
+                    else:
+                        start = start - steps_left
+                        steps = steps + 1
+                else:
+                    start = start + steps_right
+                    steps = steps + 1
+            else:
+                steps = steps + ((distance-mod_distance)/steps_right)
+                start = steps*steps_right + start
+                if start > no_of_states:
+                    if start <= steps_left:
+                        es = 1
+                    else:
+                        start = start - steps_left
+                        steps = steps + 1
+    else:
+        goal, start = start, goal
+        steps_right, steps_left = steps_left, steps_right
+
+
+if es == 1:
+    print("use the stairs")
+else:
+    print(int(steps))
 
 
